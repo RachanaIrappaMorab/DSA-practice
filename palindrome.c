60ca4b5bbc453e375dca1f224be9ba3e300d2f70
RACHANA IRAPPA MORAB <rachanamorab@gmail.com>
2026-02-26 19:32:20 +0530

    added palindrome solution

---
+++ palindrome.c
@@ -0,0 +1,16 @@
+bool isPalindrome(int x)
+ { 
+    if(x<0) 
+    return false; 
+    if (x % 10 == 0 && x != 0) 
+        return false;
+    
+       int rev=0; 
+        while(x>rev){ 
+    int last=x%10;
+       rev=(rev*10)+last;
+         x=x/10;
+          }
+         return (x == rev || x == rev / 10);
+  
+}  
