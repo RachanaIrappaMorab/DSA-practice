4a0e2f6632157548c07852d76c136361aae8697e
RACHANA IRAPPA MORAB <rachanamorab@gmail.com>
2026-03-01 09:47:10 +0530

    Add missingNumber function to identify missing numbers
    Implement function to find the missing number in an array.

---
+++ array/missingnum.c
@@ -0,0 +1,14 @@
+// sum of the num from 0 to n 
+//total=n*(n+1)/2
+//sum of theall elements in an array 
+//missing number=total-sum
+
+int missingNumber(int* nums, int numsSize) {
+    int p=numsSize;
+      int tot=p*(p+1)/2;
+       int sum=0; 
+       for(int i=0;i<numsSize;i++){
+         sum=sum+nums[i];}  
+         int miss=tot-sum;
+           return miss; 
+} 
