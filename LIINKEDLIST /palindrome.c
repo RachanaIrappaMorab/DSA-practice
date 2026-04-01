a02f8938093d3c39d51bc287aabcd4257fb10874
RACHANA IRAPPA MORAB <rachanamorab@gmail.com>
2026-04-01 19:01:41 +0530

    Add palindrome check for linked list
    Implement a function to check if a linked list is a palindrome.

---
+++ LIINKEDLIST /palindrome.c
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+bool isPalindrome(struct ListNode* head) {
+    int arr[100000];
+    int n=0;
+    struct ListNode* temp=head;
+    while(temp!=NULL){
+        arr[n]=temp->val;
+        n++;
+        temp=temp->next;
+    }
+    int i=0;
+    int j=n-1;
+    while(i<j){
+        if(arr[i]!=arr[j]){
+            return false;
+        }
+        i++;
+        j--;
+    }
+    return true;
+    
+}
