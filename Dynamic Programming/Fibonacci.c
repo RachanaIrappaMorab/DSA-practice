de340363cf757dafc999da521b25c74d6e3aed43
RACHANA IRAPPA MORAB <rachanamorab@gmail.com>
2026-04-03 19:11:26 +0530

    Create Fibonacci.c

---
+++ Dynamic Programming/Fibonacci.c
@@ -0,0 +1,22 @@
+//using memoization technique 
+
+int dp[10000];
+int solve(int n){
+ 
+  if(n==0||n==1){
+    return n;
+  }
+  if(dp[n]!=-1){
+    return dp[n];
+  }  
+  dp[n]=solve(n-1)+solve(n-2);
+  return dp[n];
+
+}
+int fib(int n) {
+    for(int i = 0; i <= n; i++){
+        dp[i] = -1;
+    }
+
+    return solve(n);
+}
